26bfb1d9594d4f1b804854ffbe79d29ac980345c
Hang Nguyen <hangnguyen1998.vh@gmail.com>
2024-07-17 17:28:31 +0900

    Add project files.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using bbs.Data;
+using bbs.Models;
+
+namespace bbs.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        {
+            var currentUser = User.Identity.Name;
+            var viewModel = new PostModelView
+            {
+                SortOrder = sortOrder,
+                CurrentUser = currentUser
+            };
+            IQueryable<Post> postQuery = _context.Post;
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                postQuery = postQuery.Where(p => p.Content.Contains(searchString) || p.UserName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "content_asc":
+                    postQuery = postQuery.OrderBy(p => p.Content);
+                    break;
+                case "content_desc":
+                    postQuery = postQuery.OrderByDescending(p => p.Content);
+                    break;
+                case "userName_asc":
+                    postQuery = postQuery.OrderBy(p => p.UserName);
+                    break;
+                case "userName_desc":
+                    postQuery = postQuery.OrderByDescending(p => p.UserName);
+                    break;
+                case "createdAt_asc":
+                    postQuery = postQuery.OrderBy(p => p.CreatedAt);
+                    break;
+                case "createdAt_desc":
+                    postQuery = postQuery.OrderByDescending(p => p.CreatedAt);
+                    break;
+                default:
+                    postQuery = postQuery.OrderByDescending(p => p.CreatedAt);
+                    break;
+            }
+            viewModel.Posts = await postQuery.AsNoTracking().ToListAsync();
+            return View(viewModel);
+        }
+
+        // GET: Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Content,UserId,UserName,CreatedAt")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Content,UserId,UserName,CreatedAt")] Post post)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Post.FindAsync(id);
+            if (post != null)
+            {
+                _context.Post.Remove(post);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Post.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/PostModelView.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+
+namespace bbs.Models
+{
+    public class PostModelView
+    {
+        public List<Post> Posts { get;set; }
+        public string SortOrder { get; set; }
+        public IEnumerable<SelectListItem> SortOptions { get; set; }
+        public string SearchString { get; set; }
+        public string CurrentUser { get; set; }
+
+        public PostModelView()
+        {
+            SortOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "content_asc", Text = "内容（昇順）" },
+                new SelectListItem { Value = "content_desc", Text = "内容（降順）" },
+                new SelectListItem { Value = "userName_asc", Text = "ユーザー（昇順）" },
+                new SelectListItem { Value = "userName_desc", Text = "ユーザー（降順）" },
+                new SelectListItem { Value = "createdAt_asc", Text = "投稿時間（昇順）" },
+                new SelectListItem { Value = "createdAt_desc", Text = "投稿時間（降順）" }
+            };
+        }
+    }
+}
